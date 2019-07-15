0c60ad41632fb888b772b99a971db0a1c30cec5f
Yuri Morales <paladin2005@gmail.com>
2019-07-15 16:41:02 -0400

    New Collection V2 and Chart fixes missing migration files

---
+++ AbaBackend/Migrations/20190628174319_SupervisionNoteReject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbaBackend.Migrations
+{
+  public partial class SupervisionNoteReject : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AddColumn<string>(
+        name: "RejectNotes",
+        table: "SessionSupervisionNotes",
+        nullable: true);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropColumn(
+        name: "RejectNotes",
+        table: "SessionSupervisionNotes");
+    }
+  }
+}
